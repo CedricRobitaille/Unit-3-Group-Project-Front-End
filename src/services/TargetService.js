e11fe86c1b9a52d6cd4bf535e1d618209c96ba17
Cedric <crubbertire@rogers.com>
2025-11-14 11:54:14 -0500

    Created TargetService Fetching

--- src/services/TargetService.js
+++ src/services/TargetService.js
@@ -1,40 +1,101 @@
 const BASE_URL = "http://localhost:3000/targets/";
 
+/**
+ * Collect all targets currrently in the user's watchlist
+ * @returns All watchlist targets
+ */
 const index = async () => {
   try {
-
+    const response = await fetch(BASE_URL);
+    const data = await response.json()
+    console.log("Fetched Watchlist Targets: ", data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-const create = async () => {
-  try {
 
+
+/**
+ * Adds the target ticker to the user's watchlist
+ * @param {Object} targetName The target ticker's name
+ * @returns The added target's data
+ */
+const create = async (targetName) => {
+  try {
+    const response = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(targetName),
+    });
+    const data = await response.json();
+    console.log("Added Target to Watchlist: ", data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-const show = async () => {
-  try {
 
+
+/**
+ * Fetches a single target from the user's watchlist
+ * @param {String} targetId Name of the Target in question
+ * @returns Single target Information
+ */
+const show = async (targetId) => {
+  try {
+    const response = await fetch(BASE_URL + targetId);
+    const data = await response.json()
+    console.log(`Fetched Watchlist Target info for ${targetId}: `, data);
+    return data;
+  } catch (error) {
+    console.log("ERROR: Target does not exist in watchlist.", error);
+  }
+}
+
+
+
+/**
+ * Updates the target in the watchlist
+ * @param {String} targetId Name of the target in question
+ * @param {String} targetData tickerName
+ * @returns Updated Transaction Details
+ */
+const update = async (targetId, targetData) => {
+  try {
+    const response = await fetch(`${BASE_URL}${targetId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(targetData),
+    });
+    const data = await response.json();
+    console.log(`Updated transaction ${targetId}: `, data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-const update = async () => {
+
+
+/**
+ * Deletes the target ticker from user's watchlist
+ * @param {String} targetId ID of the transaction in question
+ * @returns Deleted Transaction Information
+ */
+const del = async (targetId) => {
   try {
-
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-const del = async () => {
-  try {
-
+    const response = await fetch(`${BASE_URL}${targetId}`, {
+      method: "DELETE"
+    });
+    const data = await response.json();
+    console.log(`Deleted Target ${targetId} from Watchlist: `, data);
   } catch (error) {
     console.log(error);
   }
