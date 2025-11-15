ad29e4fcefa970bb5097408285a5780ffc35e198
justinclark-dev <justinclark.dev@gmail.com>
2025-11-15 14:51:17 -0800

    Integrates SideNav and TopNav components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 
+import { useState } from 'react';
 import './App.css'
-
+import SideNav from './components/Nav/SideNav/SideNav'
+import TopNav from './components/Nav/TopNav/TopNav';
 import Watchlist from './components/Watchlist/Watchlist';
 
 function App() {
 
+  const [currentSection, setCurrentSection] = useState('General');
+  const [currentPage, setCurrentPage] = useState('Dashboard');
+
+  const handleNavigation = (section, page) => {
+    setCurrentSection(section);
+    setCurrentPage(page);
+  };
+
   return (
     <>
-      <h1>Financial App Front-End</h1>
-      <Watchlist />
+      <SideNav 
+        currentPage={currentPage} 
+        handleNavigation={handleNavigation}
+      />
+      <main>
+        <TopNav 
+          currentSection={currentSection} 
+          currentPage={currentPage} 
+        />
+        <section>
+          <Watchlist />
+        </section> 
+      </main>
     </>
   )
 }
